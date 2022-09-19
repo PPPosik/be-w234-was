3ff424437817a8d2cd372fce7bd5b12508f946d7
pposik.p <pposik.p@kakaocorp.com>
2022-09-19 11:19:20 +0900

    메서드에 따른 요청 처리를  분리

--- src/main/java/webserver/servicehandler/SignUpServiceHandler.java
+++ src/main/java/webserver/servicehandler/SignUpServiceHandler.java
@@ -1,10 +1,8 @@
 package webserver.servicehandler;
 
+import exception.RequestParsingException;
 import model.User;
-import util.Mime;
-import util.Request;
-import util.Response;
-import util.ResponseEntity;
+import util.*;
 import webserver.service.SignUpService;
 
 public class SignUpServiceHandler implements ServiceHandler {
@@ -16,6 +14,18 @@ public class SignUpServiceHandler implements ServiceHandler {
 
     @Override
     public Response handle(Request request) {
+        HttpMethod method = HttpMethod.getByMethod(request.getMethod());
+
+        if (method == HttpMethod.GET) {
+            return getHandle(request);
+        } else if (method == HttpMethod.POST) {
+            return postHandle(request);
+        } else {
+            throw new RequestParsingException(request.getMethod() + " 은 지원하지 않는 메서드입니다.");
+        }
+    }
+
+    private Response getHandle(Request request) {
         final String userId = request.getParams().get("userId");
         final String password = request.getParams().get("password");
         final String name = request.getParams().get("name");
@@ -24,10 +34,10 @@ public class SignUpServiceHandler implements ServiceHandler {
         User user = new User(userId, password, name, email);
         ResponseEntity entity = signUpService.service(user);
 
-        return new Response()
-                .setHttpStatusCode(entity.getHttpStatusCode())
-                .setHeader("Content-Length", String.valueOf(entity.getBody().length))
-                .setHeader("Content-Type", Mime.getContentType(request.getPath(), request.getHeaders().get("accept")) + ";charset=utf-8")
-                .setBody(entity.getBody());
+        return new Response().setHttpStatusCode(entity.getHttpStatusCode()).setHeader("Content-Length", String.valueOf(entity.getBody().length)).setHeader("Content-Type", Mime.getContentType(request.getPath(), request.getHeaders().get("accept")) + ";charset=utf-8").setBody(entity.getBody());
+    }
+
+    private Response postHandle(Request request) {
+        return new Response().setHttpStatusCode(HttpStatusCode.CREATED).setBody("created");
     }
 }
